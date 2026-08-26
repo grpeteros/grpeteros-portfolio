d62140edb85fd69a8a2ccdfb62bc934b3627ffce
Gaius Peteros <grpeteros06@gmail.com>
2026-08-26 18:30:28 +0800

    Initial deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig= {
   /* config options here */
+  output: "export",
 };
+
+// export default nextConfig;
 
 export default nextConfig;
