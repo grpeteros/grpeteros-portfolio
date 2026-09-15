50e02f612b33515ef35302614c7a66f4c46a7bcc
Gaius Peteros <grpeteros06@gmail.com>
2026-09-15 13:41:54 +0800

    Added delete from cart

--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -29,18 +29,34 @@ export default function SalesPage() {
     <div
       className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black"
     >
-        <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start" 
-        >
-          <div>
-            <Typography variant="h3" component="h1" gutterBottom>
-              Total sales for the last 30 days
-            </Typography> 
-            <Typography variant="h2" component="h1" gutterBottom>
-             { `${new Date(new Date().setDate(new Date().getDate() - 30)).toLocaleDateString()} ~ ${new Date().toLocaleDateString()}`} PHP{totalPrice.toFixed(2)}
-            </Typography> 
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start"
+      >
+        <div>
+          <Typography variant="h2" component="h1" gutterBottom>
+            Total sales for the last 30 days
+          </Typography>
+          <Typography variant="h3" component="h3" gutterBottom>
+            {`${new Date(new Date().setDate(new Date().getDate() - 30)).toLocaleDateString()} ~ ${new Date().toLocaleDateString()}`}
+          </Typography>
+          <Typography variant="h4" component="h4" gutterBottom className="text-gray-600 dark:text-gray-400 justify-center">
+            Total Sales:
+            PHP{totalPrice.toFixed(2)}
+          </Typography>
 
-          </div>
-        </main>
+
+          {data?.map((transaction: any, index: number) => (
+            <div key={index}>
+              {transaction.products.map((product: any, productIndex: number) => (
+                <div key={productIndex}>
+                  <Typography variant="h6" component="h6" gutterBottom className="text-gray-600 dark:text-gray-400 justify-center">
+                    {`(${new Date(transaction.created_at).toLocaleDateString()}) ${product.product_name} - PHP${product.product_price} x ${product.product_quantity} = PHP${(Number(product.product_price) * Number(product.product_quantity)).toFixed(2)}`}
+                  </Typography>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
